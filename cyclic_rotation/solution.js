5938301b247951785690e04bf3f71088c27b0625
Djieuga Djoubi <djieugadjoubirylenathan@gmail.com>
2024-06-07 10:10:12 +0100

    [function]: rotateAntiClockWise -> rotate a list in anti-clockwise direction

--- cyclic_rotation/solution.js
+++ cyclic_rotation/solution.js
@@ -37,7 +37,7 @@ function rotateArray(nums, k) {
   const cycles = gcd(n, k);
 
   // Perform rotation for each cycle
-  for (let i = 0; i < cycles; ++i) {
+  for (let i = 0; i < cycles; i++) {
     let start = i; // Start index of the current cycle
     let current = start; // Current index being processed
     let prev = nums[start]; // Store the value to be replaced
@@ -60,6 +60,69 @@ function rotateArray(nums, k) {
   return nums; // Return the rotated array
 }
 
+function rotateList(A, K) {
+  const n = A.length;
+  K = K % n;
+  let b = K;
+  if (K === 0) {
+    return A;
+  }
+  for (let i = 0; i < K; i++) {}
+  let cycles = n;
+  while (b !== 0) {
+    let tmp = b;
+    b = cycles % b;
+    cycles = tmp;
+  }
+  for (let i = 0; i < cycles; i++) {
+    let start = i;
+    let current = start;
+    let prev = A[start];
+    while (true) {
+      let nextIdx = (current + K) % n;
+      let tmp = A[nextIdx];
+      A[nextIdx] = prev;
+      prev = tmp;
+      current = nextIdx;
+      console.log(A + " kept:" + prev);
+      if (nextIdx === start) {
+        break;
+      }
+    }
+  }
+  return A;
+}
 
+const rotateAntiClockWise = (A, K) => {
+  let n = A.length;
+  K = K % n; // make sure K remain within the scope of the list
+  //do no rotate if K is 0
+  if (K === 0) {
+    return A;
+  }
+  let cycles = gcd(n, K);
+  for (let i = cycles; i > 0; i--) {
+    let start = n-1;
+    let current = start;
+    let prev = A[current];
+    console.log("Loop: "+i)
+    while (true) {
+      let nextIdx = (current - K) % n;
+      if(nextIdx < 0){
+        nextIdx = n + nextIdx
+      }
+      let tmp = A[nextIdx]
+      A[nextIdx] = prev
+      prev = tmp
+      current = nextIdx
+      console.log("["+A+"] -> kept: "+ prev + " in temporary memory")
+      if (nextIdx === start) {
+        break;
+      }
+    }
+  }
+  return A;
+};
 
-console.log(rotateList([3, 8, 9, 7, 6], 1));
+// console.log(rotateList([3, 8, 9, 7, 6], 1));
+console.log(rotateAntiClockWise([3, 8, 9, 7, 6], 1));
